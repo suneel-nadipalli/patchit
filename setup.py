bca213503f5dc8ca2c0dc00856916f3a5ef13ebc
suneel-nadipalli <suneel.nadipalli@duke.edu>
2024-11-25 04:13:06 -0500

    Changed Package name

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="patchit",  # Your package name
+    name="patchviz",  # Your package name
     version="1.0.0",  # Package version
-    description="Patch It: A library to test how well your model holds up against adversarial attacks",  # Short description
+    description="Patch Viz: A library to test how well your model holds up against adversarial attacks",  # Short description
     long_description=open("README.md").read(),  # Use README.md for detailed description
     long_description_content_type="text/markdown",
     author="Suneel Nadipalli",  # Replace with your name
